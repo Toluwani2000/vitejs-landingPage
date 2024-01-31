433268a46d9bf576175c481621c6960de8d76800
Toluwani <toluadtech@gmail.com>
2024-01-31 15:35:46 +0100

    dist after base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/Toluwani2000/vitejs-landingPage.git/",
+  base: "/https://toluwani2000.github.io/vitejs-landingPage//",
   plugins: [react()],
 })
